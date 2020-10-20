aa16ae26132dd14c8cb131c3bb6c30e9c77b713e
ricardotmendes <ricardo.cau.mendes@gmail.com>
2020-10-20 08:38:12 -0300

    Deploy

--- Dia 19.10/blogPessoal/src/main/java/generation/org/blogPessoal/repository/UsuarioRepository.java
+++ Dia 19.10/blogPessoal/src/main/java/generation/org/blogPessoal/repository/UsuarioRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	
 }
